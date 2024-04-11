e09f1b93ca5cdf176555ba14e7b4bb4a2e29470d
KirillKaszycki <kashitskiikirill@gmail.com>
2024-04-11 13:20:29 +0500

    added the profile screen

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Кирилл Кашицкий on 11.04.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
