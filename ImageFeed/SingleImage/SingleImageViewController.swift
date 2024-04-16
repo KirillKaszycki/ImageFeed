bff9416a8cf353c8b89696ad3090226536afa118
KirillKaszycki <kashitskiikirill@gmail.com>
2024-04-16 10:20:21 +0500

    Refactored the single image view controller, added the share logics, added the zoom logics

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Кирилл Кашицкий on 11.04.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+
+    @IBOutlet private var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+}
