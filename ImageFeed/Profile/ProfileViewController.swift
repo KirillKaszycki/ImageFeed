3e69d014fb853635a62ea5f684b91a091badb646
KirillKaszycki <kashitskiikirill@gmail.com>
2024-04-20 10:02:55 +0500

    Configured the profileViewController with code

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -8,14 +8,86 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
-
-    @IBOutlet private var logoutButton: UIButton!
-
-    @IBAction private func didTapLogoutButton() {
+    
+    private var profilePhoto = UIImage()
+    
+    private var nameLabel = UILabel()
+    private var loginLabel = UILabel()
+    private var descriptionLabel = UILabel()
+    
+    private var exitButton = UIButton()
+    
+    private let imageView = UIImageView(image: .avatar)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profileImageConfigure()
+        labelsConfigure()
+        exitButtonConfigure()
     }
+    
+    private func profileImageConfigure() {
+        // Profile Image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+    }
+    
+    private func labelsConfigure() {
+        // Labels
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = .ypWhite
+        nameLabel.font = .systemFont(ofSize: 23, weight: .bold)
+        
+        loginLabel.text = "@ekaterina_nov"
+        loginLabel.textColor = .ypGray
+        loginLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = .ypGray
+        descriptionLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        
+        
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        
+        nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+            
+        loginLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+            
+        descriptionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    private func exitButtonConfigure() {
+        // Quit Button
+        let button = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(Self.didTapButton)
+        )
+        
+        button.tintColor = .ypRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        
+        button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+        
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+    }
+    
+    @objc
+    private func didTapButton() {}
 }
-
