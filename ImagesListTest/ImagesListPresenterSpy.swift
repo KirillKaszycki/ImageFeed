86eb19b16941fd9b6f813d493c3164c32b2618ae
KirillKaszycki <kashitskiikirill@gmail.com>
2024-06-21 16:21:41 +0500

    Added tests for ImagesList

--- ImagesListTest/ImagesListPresenterSpy.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ImagesListPresenterSpy.swift
-//  ImagesListTest
-//
-//  Created by Кирилл Кашицкий on 21.06.2024.
-//
-
-import Foundation
--- ImagesListTest/ImagesListTest.swift
+++ ImagesListTest/ImagesListTest.swift
@@ -6,30 +6,57 @@
 //
 
 import XCTest
+@testable import ImageFeed
 
-final class ImagesListTest: XCTestCase {
+class ImagesListPresenterTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var presenter: ImagesListViewPresenter!
+    var spy: ImagesListControllerSpy!
+    
+    override func setUp() {
+        super.setUp()
+        spy = ImagesListControllerSpy()
+        presenter = ImagesListViewPresenter(view: spy)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        presenter.removeObserver()
+        presenter = nil
+        spy = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testUpdateTableViewAnimatedCalled() {
+        // Given
+        let oldCount = 0
+        let newPhotos = [
+            Photo(
+                id: "1",
+                size: CGSize(width: 100, height: 100),
+                createdAt: "2024-06-21",
+                welcomeDescription: "Description",
+                thumbImageURL: URL(string: "https://example")!,
+                fullImageURL: URL(string: "https://example")!,
+                isLiked: false
+            ),
+            Photo(
+                id: "2",
+                size: CGSize(width: 200, height: 200),
+                createdAt: "2024-06-22",
+                welcomeDescription: "Description",
+                thumbImageURL: URL(string: "https://example")!,
+                fullImageURL: URL(string: "https://example")!,
+                isLiked: true
+            )
+        ]
+        
+        // When
+        presenter.photos = newPhotos
+        presenter.updateTableViewAnimated()
+        
+        // Then
+        XCTAssertTrue(spy.isUpdateTableViewAnimatedCalled)
+        XCTAssertNotEqual(spy.updateTableViewAnimatedArguments?.oldCount, oldCount)
+        XCTAssertNotEqual(spy.updateTableViewAnimatedArguments?.newCount, newPhotos.count)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
